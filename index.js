f9fa5743c6514906083af10ad9bb057ab71ac127
Justice Oppong Tuah <163655889+Opt-Jay@users.noreply.github.com>
2025-12-28 23:47:20 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const conversions = {
+    metersToFeet: function(value) {return value * 3.281},
+    feetToMeters: function(value) {return value / 3.281},
+    litresToGallons: function(value) {return value * 0.264},
+    gallonsToLitres: function(value) {return value / 0.264},
+    kilosToPounds: function(value) {return value * 2.204},
+    poundsToKilos: function(value) {return value / 2.204}
+}
+
+
+let inputEl = document.getElementById("input")
+let convertEl = document.getElementById("convert-btn")
+
+let lenOutput = document.getElementById("len-output")
+let volOutput = document.getElementById("vol-output")
+let massOutput = document.getElementById("mass-output")
+let convertBtn = document.getElementById("convert-btn")
+
+
+//BUTTON CLICK EVENT
+convertBtn.addEventListener("click", convertNumber)
+
+//ENTER KEY EVENT
+inputEl.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        convertNumber()
+    }
+})
+
+
+function convertNumber(){
+    //GET INPUT AS A NUMBER
+    const value = parseFloat(inputEl.value)
+    if (!value){
+        alert("Please enter a number!")
+        return
+    } 
+    //LENGTH CONVERSION
+    const metersToFeetResult = conversions.metersToFeet(value).toFixed(3)
+    const feetToMetersResult = conversions.feetToMeters(value).toFixed(3)
+    lenOutput.textContent = `${value} metres = ${metersToFeetResult} feet | 
+                                ${value} feet =  ${feetToMetersResult} meters`
+
+    //VOLUME CONVERSION
+    const litresToGallonsResult = conversions.litresToGallons(value).toFixed(3)
+    const gallonsToLitresResult = conversions.gallonsToLitres(value).toFixed(3)
+    volOutput.textContent = `${value} liters = ${litresToGallonsResult} gallons | 
+                                ${value} gallons = ${gallonsToLitresResult} gallons`
+
+    //MASS CONVERSION
+    const kilosToPoundsResult = conversions.kilosToPounds(value).toFixed(3)
+    const poundsToKilosResult = conversions.poundsToKilos(value).toFixed(3)
+    massOutput.textContent = `${value} kilos = ${kilosToPoundsResult} pounds | 
+                                ${value} pounds = ${poundsToKilosResult} kilos`
+}
+
+
+
